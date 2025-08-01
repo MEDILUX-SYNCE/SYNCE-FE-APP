f613cc6d0b52c368636b08912ba17034553f43c9
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-08-01 16:27:39 +0900

    feat: complete AlertScreen UI design

--- src/screens/my/alert/AlertScreen.tsx
+++ src/screens/my/alert/AlertScreen.tsx
@@ -1,5 +1,113 @@
+import React, { useState } from 'react';
+import { ScrollView, View, StyleSheet, Switch } from 'react-native';
 import { TopNavigation } from '../../../navigation/TopNavigation';
+import { colors } from '../../../theme/color';
+import { AppText } from '../../../components/AppText';
+import { AppSwitch } from '../../../components/AppSwitch';
 
 export default function AlertScreen() {
-  return <TopNavigation title={'알림 설정'} hasBack hasCancel={false} />;
+  // 스위치 상태 관리
+  const [isCounselingEnabled, setIsCounselingEnabled] = useState(false);
+  const [isChecklistEnabled, setIsChecklistEnabled] = useState(true);
+  const [isRecordEnabled, setIsRecordEnabled] = useState(true);
+  const [isArticleEnabled, setIsArticleEnabled] = useState(true);
+
+  // 전체 알림 설정 변수
+  const isAllEnabled =
+    isCounselingEnabled &&
+    isChecklistEnabled &&
+    isRecordEnabled &&
+    isArticleEnabled;
+
+  // 전체 알림 설정
+  const toggleAll = () => {
+    const newValue = !isAllEnabled;
+    setIsCounselingEnabled(newValue);
+    setIsChecklistEnabled(newValue);
+    setIsRecordEnabled(newValue);
+    setIsArticleEnabled(newValue);
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <TopNavigation title="알림설정" hasBack hasCancel={false} />
+
+      <View style={styles.topCard}>
+        {/* 전체 알림 */}
+        <View style={styles.row}>
+          <AppText color="black" size="md" weight="medium">
+            전체 알림
+          </AppText>
+          <AppSwitch value={isAllEnabled} onValueChange={toggleAll} />
+        </View>
+      </View>
+
+      {/* 개별 알림 */}
+      <View style={styles.card}>
+        <View style={styles.row}>
+          <AppText color="black" size="md" weight="medium">
+            주치의 상담 알림
+          </AppText>
+          <AppSwitch
+            value={isCounselingEnabled}
+            onValueChange={setIsCounselingEnabled}
+          />
+        </View>
+
+        <View style={styles.row}>
+          <AppText color="black" size="md" weight="medium">
+            오늘의 체크리스트 알림
+          </AppText>
+          <AppSwitch
+            value={isChecklistEnabled}
+            onValueChange={setIsChecklistEnabled}
+          />
+        </View>
+
+        <View style={styles.row}>
+          <AppText color="black" size="md" weight="medium">
+            기록장 알림
+          </AppText>
+          <AppSwitch
+            value={isRecordEnabled}
+            onValueChange={setIsRecordEnabled}
+          />
+        </View>
+
+        <View style={styles.row}>
+          <AppText color="black" size="md" weight="medium">
+            아티클 알림
+          </AppText>
+          <AppSwitch
+            value={isArticleEnabled}
+            onValueChange={setIsArticleEnabled}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  topCard: {
+    backgroundColor: colors.white,
+    borderBottomWidth: 7,
+    borderBottomColor: colors.whitegray,
+  },
+  card: {
+    backgroundColor: colors.white,
+  },
+  row: {
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: colors.whitegray,
+  },
+});
