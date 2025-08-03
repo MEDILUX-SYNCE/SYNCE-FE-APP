e7ec4426cdc3da79ef6c8ff7f8d1ec81f9be7d50
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-08-03 17:48:57 +0900

    feat: change Terms of Service to modal format and update agreement order

--- src/screens/onboarding/OnboardingScreen.tsx
+++ src/screens/onboarding/OnboardingScreen.tsx
@@ -3,8 +3,8 @@ import { useNavigation } from '@react-navigation/native';
 import {
   Dimensions,
   FlatList,
-  GestureResponderEvent,
   Image,
+  Modal,
   SafeAreaView,
   StyleSheet,
   View,
@@ -14,6 +14,9 @@ import { AppButton } from '../../components/AppButton';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../navigation/RootStackParamList';
 import { colors } from '../../theme/color';
+import { TouchableOpacity } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { AppModal } from '../../components/AppModal';
 
 // 화면 너비, 높이 가져오기 (페이지 단위 스크롤)
 const { width, height } = Dimensions.get('window');
@@ -41,6 +44,61 @@ const TitleData = [
 ];
 
 export default function OnboardingScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [agree14, setAgree14] = useState(false);
+  const [agreeService, setAgreeService] = useState(false);
+  const [agreePrivacy, setAgreePrivacy] = useState(false);
+  const [agreeServiceModal, setAgreeServiceModal] = useState(false);
+  const [agreePrivacyModal, setAgreePrivacyModal] = useState(false);
+
+  const allChecked = agree14 && agreeService && agreePrivacy;
+  const toggleAll = () => {
+    const next = !allChecked;
+    setAgree14(next);
+    setAgreeService(next);
+    setAgreePrivacy(next);
+  };
+
+  // AgreementItem
+  const AgreementItem = ({
+    checked,
+    onPress,
+    label,
+    bold,
+    highlight,
+  }: {
+    checked: boolean;
+    onPress: () => void;
+    label: React.ReactNode;
+    bold?: boolean;
+    highlight?: boolean;
+  }) => (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.agreementItem}>
+        <View style={[styles.checkboxBase, checked && styles.checkboxChecked]}>
+          {checked && <MaterialIcons name="check" size={16} color="white" />}
+        </View>
+        <AppText
+          style={[
+            styles.label,
+            bold && styles.boldLabel,
+            {
+              color: highlight
+                ? checked
+                  ? colors.primary1
+                  : colors.gray3
+                : colors.black,
+              textDecorationLine: highlight && checked ? 'underline' : 'none',
+            },
+          ]}
+        >
+          {label}
+        </AppText>
+      </View>
+    </TouchableOpacity>
+  );
+
   // 네비게이션 객체 사용
   type OnboardingScreenNavigationProp = NativeStackNavigationProp<
     RootStackParamList,
@@ -81,7 +139,7 @@ export default function OnboardingScreen() {
 
   const handNext = (index: number) => {
     if (index === TitleData.length - 1) {
-      navigation.navigate('Signup');
+      setModalVisible(true);
     } else {
       flatListRef.current?.scrollToIndex({ index: index + 1 });
     }
@@ -159,6 +217,144 @@ export default function OnboardingScreen() {
           />
         )}
       </View>
+
+      {/* 약간 동의 모달 */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            {/* 타이틀 */}
+            <View
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+            >
+              <AppText color="black" size="lg" weight="bold">
+                서비스 이용 약관에 동의해주세요.
+              </AppText>
+              <TouchableOpacity onPress={() => setModalVisible(false)}>
+                <Image
+                  source={require('../../assets/images/icons/cancel.png')}
+                  style={{ width: 24, height: 24 }}
+                />
+              </TouchableOpacity>
+            </View>
+
+            {/* 약관 체크박스 */}
+            <View style={{ marginTop: 16, marginBottom: 32 }}>
+              <AgreementItem
+                checked={allChecked}
+                onPress={toggleAll}
+                label="모두 동의"
+              />
+              <View style={styles.divider} />
+              <AgreementItem
+                checked={agree14}
+                onPress={() => setAgree14(!agree14)}
+                label={
+                  <AppText
+                    size="sm"
+                    weight="medium"
+                    style={{
+                      color: agree14 ? colors.gray4 : colors.gray3,
+                    }}
+                  >
+                    (필수) 만 14세 이상입니다
+                  </AppText>
+                }
+              />
+              <View style={{ flexDirection: 'row' }}>
+                <AgreementItem
+                  checked={agreeService}
+                  onPress={() => setAgreeService(!agreeService)}
+                  label={
+                    <>
+                      <AppText
+                        size="sm"
+                        weight="medium"
+                        style={{
+                          color: agreeService ? colors.gray4 : colors.gray3,
+                        }}
+                      >
+                        (필수){' '}
+                      </AppText>
+                      <AppText
+                        size="sm"
+                        weight="medium"
+                        style={{
+                          color: agreeService ? colors.primary1 : colors.gray3,
+                        }}
+                        onPress={() => setAgreeServiceModal(true)}
+                      >
+                        서비스 이용약관 확인
+                      </AppText>
+                    </>
+                  }
+                  highlight
+                />
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <AgreementItem
+                  checked={agreePrivacy}
+                  onPress={() => setAgreePrivacy(!agreePrivacy)}
+                  label={
+                    <>
+                      <AppText
+                        size="sm"
+                        weight="medium"
+                        style={{
+                          color: agreePrivacy ? colors.gray4 : colors.gray3,
+                        }}
+                      >
+                        (필수){' '}
+                      </AppText>
+                      <AppText
+                        size="sm"
+                        weight="medium"
+                        style={{
+                          color: agreePrivacy ? colors.primary1 : colors.gray3,
+                        }}
+                        onPress={() => setAgreePrivacyModal(true)}
+                      >
+                        개인정보 수집이용 동의
+                      </AppText>
+                    </>
+                  }
+                  highlight
+                />
+              </View>
+
+              {/* 동의 약관 내용 모달들 */}
+              <AppModal
+                title={'서비스 이용약관'}
+                content={'대충 서비스 이용약관 내용입니다.'}
+                visible={agreeServiceModal}
+                onConfirm={() => setAgreeServiceModal(false)}
+                onCancel={() => setAgreeServiceModal(false)}
+              />
+              <AppModal
+                title={'개인정보 수집이용'}
+                content={'대충 개인정보 수집이용 내용입니다.'}
+                visible={agreePrivacyModal}
+                onConfirm={() => setAgreePrivacyModal(false)}
+                onCancel={() => setAgreePrivacyModal(false)}
+              />
+            </View>
+
+            {/* 하단 버튼 */}
+            <AppButton
+              activate={agree14 && agreeService && agreePrivacy}
+              title="동의하고 계속하기"
+              onPress={() => {
+                setModalVisible(false);
+                navigation.navigate('Signup');
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -169,6 +365,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     paddingTop: height * 0.12,
+  },
+  modalContainer: {
+    height: '100%',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'flex-end',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 24,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
   },
   image: {
     width: width * 0.9,
@@ -200,5 +407,40 @@ const styles = StyleSheet.create({
   },
   inactiveDot: {
     backgroundColor: colors.gray1,
+  },
+  agreementItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 14,
+    marginLeft: 8,
+    color: colors.black,
+  },
+  boldLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  highlightLabel: {
+    color: colors.primary1,
+  },
+  checkboxBase: {
+    width: 16,
+    height: 16,
+    borderWidth: 2,
+    borderColor: colors.gray3,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkboxChecked: {
+    backgroundColor: colors.primary1,
+    borderWidth: 0,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: colors.gray1,
+    marginVertical: 12,
   },
 });
